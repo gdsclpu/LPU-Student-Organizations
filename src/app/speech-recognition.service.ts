9dd04100d7f745c36cf70a25f2b4038b9a033383
Raman Sharma <rs9463194297@gmail.com>
2022-06-11 13:30:54 +0530

    configured speech recognition

--- src/app/speech-recognition.service.ts
+++ src/app/speech-recognition.service.ts
@@ -1,54 +1,68 @@
 declare var webkitSpeechRecognition: any;
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SpeechRecognitionService {
   private recognition = new webkitSpeechRecognition();
-  isStoppedSpeechRecog = false;
+  private speechStateListener = new Subject<boolean>();
+  private isStoppedSpeechRecog: boolean = true;
+
   public text: string = '';
   tempWords: any;
 
-  constructor() {}
-
-  init() {
+  constructor() {
     this.recognition.interimResults = true;
     this.recognition.lang = 'en-IN';
 
-    this.recognition.addEventListener('result', (e: any) => {
+    this.recognition.onstart = () => {
+      console.log('Speech recognition started');
+      this.isStoppedSpeechRecog = false;
+      this.speechStateListener.next(false);
+    };
+
+    this.recognition.onresult = (e: any) => {
       const transcript = Array.from(e.results)
         .map((result: any) => result[0])
         .map((result: any) => result.transcript)
         .join('');
       this.tempWords = transcript;
       console.log(transcript);
-    });
+    };
+
+    this.recognition.onend = () => {
+      if (!this.isStoppedSpeechRecog) {
+        console.log('muted', this.isStoppedSpeechRecog);
+        this.recognition.start();
+        // console.log('Speech recognition stopped');
+        // console.log('muted', this.isStoppedSpeechRecog);
+        // this.isStoppedSpeechRecog = true;
+        // this.speechStateListener.next(true);
+        // this.recognition.stop();
+      }
+    };
+  }
+
+  getSpeechStateListener(): Observable<boolean> {
+    return this.speechStateListener.asObservable();
+  }
+
+  getText() {
+    return this.text;
+  }
+
+  getisStoppedSpeechRecog() {
+    return this.isStoppedSpeechRecog;
   }
 
   start() {
-    this.isStoppedSpeechRecog = false;
     this.recognition.start();
-    console.log('Speech recognition started');
-    this.recognition.addEventListener('end', (condition: any) => {
-      if (this.isStoppedSpeechRecog) {
-        this.recognition.stop();
-        console.log('End speech recognition');
-      } else {
-        this.wordConcat();
-        this.recognition.start();
-      }
-    });
   }
   stop() {
     this.isStoppedSpeechRecog = true;
-    this.wordConcat();
+    this.speechStateListener.next(true);
     this.recognition.stop();
-    console.log('End speech recognition');
-  }
-
-  wordConcat() {
-    this.text = this.text + ' ' + this.tempWords + '.';
-    this.tempWords = '';
   }
 }
--- src/app/voice-assistant/voice-assistant.component.ts
+++ src/app/voice-assistant/voice-assistant.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { SpeechRecognitionService } from '../speech-recognition.service';
 
 @Component({
   selector: 'app-voice-assistant',
   templateUrl: './voice-assistant.component.html',
-  styleUrls: ['./voice-assistant.component.scss']
+  styleUrls: ['./voice-assistant.component.scss'],
 })
 export class VoiceAssistantComponent implements OnInit {
+  public isStoppedSpeechRecog: boolean = true;
+  public text: string = '';
 
-  constructor() { }
+  constructor(private speechRecognitionService: SpeechRecognitionService) {}
 
   ngOnInit(): void {
+    // this.speechRecognitionService.init();
+    this.speechRecognitionService
+      .getSpeechStateListener()
+      .subscribe((value: boolean) => {
+        this.isStoppedSpeechRecog = value;
+        console.log('muted', this.isStoppedSpeechRecog);
+        this.text = this.speechRecognitionService.getText();
+      });
   }
 
+  toggleSpeechRecog() {
+    if (this.isStoppedSpeechRecog) {
+      this.isStoppedSpeechRecog = false;
+      this.speechRecognitionService.start();
+    } else {
+      this.isStoppedSpeechRecog = true;
+      this.speechRecognitionService.stop();
+    }
+  }
 }
