f34da61c9e2f09bff80df2cf00b020f333c4c305
RakeshPotnuru <rakeshpotnuru321@gmail.com>
2022-06-12 15:56:34 +0530

    Add cards and card modal
    Signed-off-by: RakeshPotnuru <rakeshpotnuru321@gmail.com>

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ModalComponent } from '../modal/modal.component';
+import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  modalRef: MdbModalRef<ModalComponent> | null = null;
 
-  constructor() { }
+  constructor(private modalService: MdbModalService) {}
 
   ngOnInit(): void {
+  }
+
+  showDetails() {
+    this.modalRef = this.modalService.open(ModalComponent)
   }
 
 }
